1040fd438bfc7554fc084a120c2df1bfeb3644eb
aportillosoto <aportillosoto@gmail.com>
2024-01-13 11:53:29 -0400

    Ajustes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <title>DevStragram - @yield('titulo')</title>
+    @vite('resources/css/app.css','resources/js/app.css')
+</head>
+<body class="bg-gray-100">
+    <header class="p-5 border-b bg-white shadow"> 
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-black">DevStagram</h1>
+        </div>
+    </header>
+</body>
+
+</html>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Página Principal
+@endsection
+@section('contenido')
+    Contenido principal
+@endsection
