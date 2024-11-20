ec8d2dc98caf7648046cae0e66f60c92649cea1b
aportillosoto <aportillosoto@gmail.com>
2024-11-20 14:58:34 -0400

    agregar user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,7 @@
                     </label>
                     <input type="text" id="name" name="name" placeholder="Ingrese su nombre" 
                     class="border p-3 w-full rounded-lg 
-                    @error('name')border-red-500 @enderror" 
+                    @error('name') border-red-500 @enderror" 
                     value="{{old('name')}}">
                     @error('name')
                         <p class="bg-red-500 text-white rounded-lg text-sm p-2 text-center">
@@ -31,7 +31,9 @@
                         Usuario
                     </label>
                     <input type="text" id="username" name="username" placeholder="Ingrese su nombre de usuario" 
-                    class="border p-3 w-full rounded-lg">
+                    class="border p-3 w-full rounded-lg 
+                    @error('username') border-red-500 @enderror" 
+                    value="{{old('username')}}">
                     @error('username')
                         <p class="bg-red-500 text-white rounded-lg text-sm p-2 text-center">
                             {{$message}}
@@ -43,7 +45,9 @@
                         Email
                     </label>
                     <input type="email" id="email" name="email" placeholder="Ingrese su correo" 
-                    class="border p-3 w-full rounded-lg">
+                    class="border p-3 w-full rounded-lg 
+                    @error('email') border-red-500 @enderror" 
+                    value="{{old('email')}}">
                     @error('email')
                         <p class="bg-red-500 text-white rounded-lg text-sm p-2 text-center">
                             {{$message}}
@@ -55,7 +59,8 @@
                         Password
                     </label>
                     <input type="password" id="password" name="password" placeholder="Ingrese su contraseña" 
-                    class="border p-3 w-full rounded-lg">
+                    class="border p-3 w-full rounded-lg 
+                    @error('password') border-red-500 @enderror"> 
                     @error('password')
                         <p class="bg-red-500 text-white rounded-lg text-sm p-2 text-center">
                             {{$message}}
