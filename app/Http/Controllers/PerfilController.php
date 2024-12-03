e4c638da84dabd0eba330411c2e9f9bb6a87a58d
aportillosoto <aportillosoto@gmail.com>
2024-12-03 16:56:12 -0400

    actualizar perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -12,5 +15,32 @@ class PerfilController extends Controller
     }
     public function index(){
         return view('perfil.index');
+    }
+    public function store(Request $request){
+        //Modificar el request
+        $request->request->add(['username'=>Str::slug($request->username)]);        
+        $this->validate($request,[
+            'username' => ['required','unique:users,username,'.auth()->user()->id,'min:3','max:20',
+            'not_in:editar-perfil'],
+            'email' => ['required','unique:users,email,'.auth()->user()->id,'email','max:60'],
+        ]);
+        if($request->imagen){
+            $imagen = $request->file('imagen');
+
+            $nombreImagen = Str::uuid().".".$imagen->extension();
+            $imagenServidor = Image::make($imagen);
+            $imagenServidor->fit(1000,1000);
+    
+            $imagenPath = public_path('perfiles').'/'.$nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->email = $request->email ?? auth()->user()->email;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? null;
+        
+        $usuario->save();
+        //redireccionar
+        return redirect()->route('posts.index',$usuario->username);
     }
 }
