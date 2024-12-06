ca3761d4ae43c049e1e8f422b7f7cdcac96c80d3
aportillosoto <aportillosoto@gmail.com>
2024-12-06 15:01:06 -0400

    bd

--- database/devstagram.sql
+++ database/devstagram.sql
@@ -32,9 +32,12 @@ CREATE TABLE `comentarios` (
   KEY `comentarios_post_id_foreign` (`post_id`),
   CONSTRAINT `comentarios_post_id_foreign` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
   CONSTRAINT `comentarios_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `comentarios` */
+
+insert  into `comentarios`(`id`,`user_id`,`post_id`,`comentario`,`created_at`,`updated_at`) values 
+(1,8,1,'un nuevo comentario','2024-12-02 21:22:23','2024-12-02 21:22:23');
 
 /*Table structure for table `failed_jobs` */
 
@@ -54,6 +57,25 @@ CREATE TABLE `failed_jobs` (
 
 /*Data for the table `failed_jobs` */
 
+/*Table structure for table `followers` */
+
+DROP TABLE IF EXISTS `followers`;
+
+CREATE TABLE `followers` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint unsigned NOT NULL,
+  `follower_id` bigint unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `followers_user_id_foreign` (`user_id`),
+  KEY `followers_follower_id_foreign` (`follower_id`),
+  CONSTRAINT `followers_follower_id_foreign` FOREIGN KEY (`follower_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `followers_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `followers` */
+
 /*Table structure for table `likes` */
 
 DROP TABLE IF EXISTS `likes`;
@@ -69,12 +91,13 @@ CREATE TABLE `likes` (
   KEY `likes_post_id_foreign` (`post_id`),
   CONSTRAINT `likes_post_id_foreign` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
   CONSTRAINT `likes_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `likes` */
 
 insert  into `likes`(`id`,`user_id`,`post_id`,`created_at`,`updated_at`) values 
-(3,8,1,'2024-12-02 17:10:29','2024-12-02 17:10:29');
+(4,8,1,'2024-12-02 21:22:32','2024-12-02 21:22:32'),
+(5,7,1,'2024-12-02 21:23:00','2024-12-02 21:23:00');
 
 /*Table structure for table `migrations` */
 
@@ -85,7 +108,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `migrations` */
 
@@ -97,7 +120,9 @@ insert  into `migrations`(`id`,`migration`,`batch`) values
 (10,'2024_11_20_173730_add_username_to_users_table',2),
 (13,'2024_11_28_205247_create_posts_table',3),
 (15,'2024_12_01_205947_create_comentarios_table',4),
-(16,'2024_12_02_161014_create_likes_table',5);
+(16,'2024_12_02_161014_create_likes_table',5),
+(17,'2024_12_03_202154_add_imagen_field_to_users_table',6),
+(18,'2024_12_05_205032_create_followers_table',7);
 
 /*Table structure for table `password_resets` */
 
@@ -175,15 +200,16 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imagen` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`,`username`) values 
-(7,'Antonio Portillo','aportillosoto@gmail.com',NULL,'$2y$10$/ttDxd8gzQpOLcM/hT2qQ./sqB0uoX8c2ArIYerx8fydYSA0vkqW.','REkvjTpFqdOpUMRcDty6RFz8PFqBvRfBcE15SkPszl4kgH1ny07TNPFHzIs3','2024-11-27 13:49:46','2024-11-27 13:49:46','aportillo'),
-(8,'Arelys Portillo','aivonneportillo@gmail.com',NULL,'$2y$10$gSJOr2jMUofZgcKB/13uVOC.p1DJBC/QegmJRn3rieH5h0SVUPRNi',NULL,'2024-11-27 18:18:05','2024-11-27 18:18:05','aivonne');
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`,`username`,`imagen`) values 
+(7,'Antonio Portillo','aportillosoto@gmail.com',NULL,'$2y$10$/ttDxd8gzQpOLcM/hT2qQ./sqB0uoX8c2ArIYerx8fydYSA0vkqW.','REkvjTpFqdOpUMRcDty6RFz8PFqBvRfBcE15SkPszl4kgH1ny07TNPFHzIs3','2024-11-27 13:49:46','2024-11-27 13:49:46','aportillo',NULL),
+(8,'Arelys Portillo','aivonneportillo@gmail.com',NULL,'$2y$10$/ttDxd8gzQpOLcM/hT2qQ./sqB0uoX8c2ArIYerx8fydYSA0vkqW.',NULL,'2024-11-27 18:18:05','2024-12-05 17:46:35','aivonne','47739ada-dc26-476f-93fc-a0651287133f.jpg');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
