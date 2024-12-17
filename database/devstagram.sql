30f1250c34ebbf73fac77e55f4373099488c71fc
aportillosoto <aportillosoto@gmail.com>
2024-12-17 08:55:37 -0400

    copia bd

--- database/devstagram.sql
+++ database/devstagram.sql
@@ -72,12 +72,16 @@ CREATE TABLE `followers` (
   KEY `followers_follower_id_foreign` (`follower_id`),
   CONSTRAINT `followers_follower_id_foreign` FOREIGN KEY (`follower_id`) REFERENCES `users` (`id`),
   CONSTRAINT `followers_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `followers` */
 
 insert  into `followers`(`id`,`user_id`,`follower_id`,`created_at`,`updated_at`) values 
-(3,8,7,NULL,NULL);
+(3,8,7,NULL,NULL),
+(4,8,9,NULL,NULL),
+(5,7,9,NULL,NULL),
+(6,7,8,NULL,NULL),
+(7,9,8,NULL,NULL);
 
 /*Table structure for table `likes` */
 
@@ -94,13 +98,16 @@ CREATE TABLE `likes` (
   KEY `likes_post_id_foreign` (`post_id`),
   CONSTRAINT `likes_post_id_foreign` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
   CONSTRAINT `likes_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `likes` */
 
 insert  into `likes`(`id`,`user_id`,`post_id`,`created_at`,`updated_at`) values 
 (4,8,1,'2024-12-02 21:22:32','2024-12-02 21:22:32'),
-(5,7,1,'2024-12-02 21:23:00','2024-12-02 21:23:00');
+(5,7,1,'2024-12-02 21:23:00','2024-12-02 21:23:00'),
+(6,8,10,'2024-12-11 14:56:18','2024-12-11 14:56:18'),
+(26,7,2,'2024-12-11 21:24:01','2024-12-11 21:24:01'),
+(30,7,11,'2024-12-13 19:12:15','2024-12-13 19:12:15');
 
 /*Table structure for table `migrations` */
 
@@ -177,7 +184,7 @@ CREATE TABLE `posts` (
   PRIMARY KEY (`id`),
   KEY `posts_user_id_foreign` (`user_id`),
   CONSTRAINT `posts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `posts` */
 
@@ -187,7 +194,10 @@ insert  into `posts`(`id`,`titulo`,`descripcion`,`imagen`,`user_id`,`created_at`
 (3,'Hoy es el día de programación','Felicidades!!!','2591aa36-ba45-425a-8314-576374f35a5f.jpg',8,'2024-11-29 18:31:14','2024-11-29 18:31:14'),
 (4,'Solucionando bugs.','Cuando no encontras el error.','c5d46505-b3ff-44d2-88d3-efff5b6e9894.jpg',8,'2024-11-29 19:44:49','2024-11-29 19:44:49'),
 (5,'Día te testing','Probando antes de levantar a producción','c38a8af3-6b03-482f-98f5-5406b0a63d98.jpg',8,'2024-11-29 19:45:23','2024-11-29 19:45:23'),
-(6,'Nuevo requerimiento','Diseñando un nuevo proceso.','a73b3623-213f-4bb9-b0f7-d7e13bfc06b7.jpg',8,'2024-11-29 19:46:11','2024-11-29 19:46:11');
+(6,'Nuevo requerimiento','Diseñando un nuevo proceso.','a73b3623-213f-4bb9-b0f7-d7e13bfc06b7.jpg',8,'2024-11-29 19:46:11','2024-11-29 19:46:11'),
+(9,'De paseo','Viaje Familiar','ddf213be-bfef-4b90-b8d9-e0ed3377875e.jpg',9,'2024-12-11 14:05:30','2024-12-11 14:05:30'),
+(10,'Urgencias HRC','El grupete','345fb195-fe44-44df-a02c-3e0beb20e9c3.jpg',9,'2024-12-11 14:06:21','2024-12-11 14:06:21'),
+(11,'Family','#','f3c5d248-cbde-4c2c-bb19-6d3a33b9db09.jpg',7,'2024-12-11 14:49:08','2024-12-11 14:49:08');
 
 /*Table structure for table `users` */
 
@@ -206,13 +216,14 @@ CREATE TABLE `users` (
   `imagen` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `users` */
 
 insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`,`username`,`imagen`) values 
-(7,'Antonio Portillo','aportillosoto@gmail.com',NULL,'$2y$10$/ttDxd8gzQpOLcM/hT2qQ./sqB0uoX8c2ArIYerx8fydYSA0vkqW.','REkvjTpFqdOpUMRcDty6RFz8PFqBvRfBcE15SkPszl4kgH1ny07TNPFHzIs3','2024-11-27 13:49:46','2024-11-27 13:49:46','aportillo',NULL),
-(8,'Arelys Portillo','aivonneportillo@gmail.com',NULL,'$2y$10$/ttDxd8gzQpOLcM/hT2qQ./sqB0uoX8c2ArIYerx8fydYSA0vkqW.',NULL,'2024-11-27 18:18:05','2024-12-05 17:46:35','aivonne','47739ada-dc26-476f-93fc-a0651287133f.jpg');
+(7,'Antonio Portillo','aportillosoto@gmail.com',NULL,'$2y$10$/ttDxd8gzQpOLcM/hT2qQ./sqB0uoX8c2ArIYerx8fydYSA0vkqW.','PwYkLhRdFOcwkLEpjxfuopkBIto0J3t398wUhXirD2Bkg9gQbm06TkMdFVTK','2024-11-27 13:49:46','2024-12-11 14:48:20','aportillo','d00398ab-3c2d-44db-8a02-435714750299.jpg'),
+(8,'Arelys Portillo','aivonneportillo@gmail.com',NULL,'$2y$10$/ttDxd8gzQpOLcM/hT2qQ./sqB0uoX8c2ArIYerx8fydYSA0vkqW.',NULL,'2024-11-27 18:18:05','2024-12-05 17:46:35','aivonne','47739ada-dc26-476f-93fc-a0651287133f.jpg'),
+(9,'Diana Romero','romerordianar@gmail.com',NULL,'$2y$10$GlBIp83s1Gs3e7o4DHb4AuYJg6BFBAp/DQJl6I.bE25C.7y.D8W2q',NULL,'2024-12-11 14:04:37','2024-12-11 14:05:01','didy','1dd11c82-144a-4acc-a987-466e6fb1b9fd.jpg');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
